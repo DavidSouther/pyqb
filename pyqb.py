8c9c80c06aa512ddf0d7449caf343f7ce8b7f8f9
David Souther <davidsouther@gmail.com>
2013-12-25 23:27:53 -0500

    Added color setting.

--- pyqb.py
+++ pyqb.py
@@ -5,3 +5,13 @@ def write(string):
 
 def cls():
     write('\033[2J')
+
+def color(fg, bg=0):
+    light = 0
+    if fg > 7:
+        light = 1
+        fg -= 8
+    bg = bg % 8
+    write('\033['+ str(light) +';3' + str(fg) + 'm')
+    write('\033[4' + str(bg) + 'm')
+
